a7f309afcec6613c8fa1de8015b1cd160e309836
Grigory Tkachenko <grtkachenko@gmail.com>
2015-03-08 16:06:56 +0300

    Initial commit

---
+++ src/dev/Indexer.java
@@ -0,0 +1,11 @@
+package dev;
+
+/**
+ * User: gtkachenko
+ * Date: 08/03/15
+ */
+public class Indexer {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/dev/Searcher.java
@@ -0,0 +1,11 @@
+package dev;
+
+/**
+ * User: gtkachenko
+ * Date: 08/03/15
+ */
+public class Searcher {
+    public static void main(String[] args) {
+
+    }
+}
